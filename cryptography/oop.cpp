27866eef6730f0cf32b85467eed827cf5ca74318
Aryan raj <95463061+ARyan0027@users.noreply.github.com>
2023-09-30 15:26:12 +0530

    Add files via upload

---
+++ cryptography/oop.cpp
@@ -0,0 +1,106 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+void encryptfunc()
+{
+    char text[100];
+    char temp;
+    int i, key;
+
+    cout << "Enter a message to encrypt: " << endl;
+    cin >> text;
+    cout << "Enter key: " << endl;
+    cin >> key;
+    for (i = 0; text[i] != '\0'; ++i)
+    {
+        temp = text[i];
+        // If the message to be encrypted is in lowercase
+        if (temp >= 'a' && temp <= 'z')
+        {
+            temp = temp + key;
+
+            if (temp > 'z')
+            {
+                temp = temp - 'z' + 'a' - 1;
+            }
+
+            text[i] = temp;
+        }
+        // If the message to be encrypted is in uppercase
+        else if (temp >= 'A' && temp <= 'Z')
+        {
+            temp = temp + key;
+
+            if (temp > 'Z')
+            {
+                temp = temp - 'Z' + 'A' - 1;
+            }
+
+            text[i] = temp;
+        }
+    }
+    cout << "Encrypted message: " << text << endl;
+}
+
+void decryptfunc()
+{
+    char text[100];
+    char temp;
+    int i, key;
+    cout << "Enter a message to decrypt: " << endl;
+    cin >> text;
+    cout << "Enter key: " << endl;
+    cin >> key;
+    for (i = 0; text[i] != '\0'; ++i)
+    {
+        temp = text[i];
+        // If the message to be decrypted is in lowercase.
+        if (temp >= 'a' && temp <= 'z')
+        {
+            temp = temp - key;
+
+            if (temp < 'a')
+            {
+                temp = temp + 'z' - 'a' + 1;
+            }
+
+            text[i] = temp;
+        }
+        // If the message to be decrypted is in uppercase.
+        else if (temp >= 'A' && temp <= 'Z')
+        {
+            temp = temp - key;
+
+            if (temp < 'A')
+            {
+                temp = temp + 'Z' - 'A' + 1;
+            }
+
+            text[i] = temp;
+        }
+    }
+    cout << "Decrypted message:" << text << endl;
+}
+
+int main()
+{
+    int choice;
+    cout << "Choose any one!" << endl;
+    cout << "1. Encryption \t 2. Decryption" << endl;
+    cin >> choice;
+    switch (choice)
+    {
+    case 1:
+        encryptfunc();
+        break;
+    case 2:
+        decryptfunc();
+        break;
+    default:
+        cout << "Please select a valid option";
+        break;
+    }
+    return 0;
+}
+
